6e9851e8b29a27cfa7493cef0be45c9c7d859b5b
mpyth0nist <devossama9@gmail.com>
2026-04-29 01:57:44 +0100

    refactoring

--- annoucements/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Announcement
-
-class AnnouncementSerializer(serializers.ModelSerializer):
-    class Meta:
-        Model = Announcement
-        fields = ['announcement_subject', 'annoucement_text', 'author']
