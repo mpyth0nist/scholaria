118c79dd1ab73f8243c7de8832e5baefb73b6d51
mpyth0nist <devossama9@gmail.com>
2026-09-14 22:14:51 +0100

    fix: CI

--- scholaria/settings.py
+++ scholaria/settings.py
@@ -130,9 +130,7 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
-        'OPTIONS': {
-            'DISABLE_SERVER_SIDE_CURSORS': True,
-        }
+        'DISABLE_SERVER_SIDE_CURSORS': True,
     }
 }
 
