08b03212bbd57aec760c4bb8e49155e53e16e1cc
mpyth0nist <devossama9@gmail.com>
2026-09-14 21:48:10 +0100

    fix: cancel streaminghttpresponse logging

--- scholaria/settings.py
+++ scholaria/settings.py
@@ -130,9 +130,6 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
-        'OPTIONS': {
-            'server_side_cursors': False,
-        }
     }
 }
 
